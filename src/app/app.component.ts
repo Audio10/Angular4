d7d713325b9c6e4779e3467c7db75a0e9d9b1197
Audio10 <claudiojesus_10@hotmail.com>
2019-09-10 13:56:56 -0500

    2-ngFor-ngIf and google maps added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'platzisquare';
-  a = 2;
-  b = 3;
-  listo = false
-  nombre:string = '';
+  title = 'platzisquare'
+  lugares:any = [
+    {active: true, nombre: "Floreria la Gardenia"},
+    {active: true, nombre: "Donas la pasadita"},
+    {active: true, nombre: "Veterinaria huellas"},
+    {active: false, nombre: "Floreria la Gardenia"},
+    {active: false, nombre: "Donas la pasadita"},
+    {active: false, nombre: "Veterinaria huellas"}
+  ]
+  lat = 51.678418;
+  lng = 7.809007;
 
   constructor(){
-    setTimeout(() => {
-      this.listo = true
-    }, 3000);
   }
 
-  hacerAlgo(){
-    alert('Haciendo algo');
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBg0uL_cxIOBUr-AIzaSyBg0uL_cxIOBUr-8Fk7wB_j6NhBFWAPJyQ'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
